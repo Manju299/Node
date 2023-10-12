34eb23986d9534274621d8c5b90a93940ffa16b8
Manjunath.Hosamani <manjunathhosamani67@gmail.com>
2023-10-13 00:01:27 +0530

    chunck file read

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const fs = require('fs')
 
 http
 .createServer(function(req,res) {
-    const text = fs.readFileSync("./Content/bigfile.txt","utf-8")
-    res.end(text)
+    // const text = fs.readFileSync("./Content/bigfile.txt","utf-8")
+    // res.end(text)
+
+    const text  = fs.createReadStream("./Content/bigfile.txt","utf-8")
+    text.on("open",() =>{
+        text.pipe(res)
+    })
+
+    text.on("error",(err) =>{
+        res.end(err);
+    })
 
 
 })
